96522f7ab456901c6d86a6861e47d58a609e0442
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2023-12-06 21:51:01 +0000

    able to run sysfile.c without undefined reference to the functions defined in other source code files

--- scheduling-xv6-lottery/tests/test_1.c
+++ scheduling-xv6-lottery/tests/test_1.c
@@ -6,27 +6,30 @@
 int
 main(int argc, char *argv[])
 {
-    struct pstat st;
 
-    if(argc != 2){
-        printf(1, "usage: mytest counter");
-        exit();
-    }
+    printf(1, "Hello World");
 
-    int i, x, l, j;
-    int mypid = getpid();
+    // struct pstat st;
 
-    for(i = 1; i < atoi(argv[1]); i++){
-        x = x + i;
-    }
+    // if(argc != 2){
+    //     printf(1, "usage: mytest counter");
+    //     exit();
+    // }
 
-    getpinfo(&st);
-    for (j = 0; j < NPROC; j++) {
-        if (st.inuse[j] && st.pid[j] >= 3 && st.pid[j] == mypid) {
-                printf(1, "pid:%d \t ticks-used:%d\n", mypid, st.ticks[j]);
-        }
-    }
+    // int i, x, l, j;
+    // int mypid = getpid();
+
+    // for(i = 1; i < atoi(argv[1]); i++){
+    //     x = x + i;
+    // }
+
+    // getpinfo(&st);
+    // for (j = 0; j < NPROC; j++) {
+    //     if (st.inuse[j] && st.pid[j] >= 3 && st.pid[j] == mypid) {
+    //             printf(1, "pid:%d \t ticks-used:%d\n", mypid, st.ticks[j]);
+    //     }
+    // }
     
-    exit();
-    return 0;
+    // exit();
+    // return 0;
 }
