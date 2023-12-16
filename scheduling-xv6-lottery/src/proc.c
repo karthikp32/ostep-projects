aede5d611bd79ab3846f4e551d744f7f53269281
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2023-12-16 20:57:37 +0000

    fixed bug where random number generator was generator same number each time, and normalized range.

--- scheduling-xv6-lottery/src/proc.c
+++ scheduling-xv6-lottery/src/proc.c
@@ -333,14 +333,13 @@ wait(void)
   }
 }
 
-static unsigned long int next = 1;
+unsigned long int next = 1;
 
 // generate random numbers in the kernel;
 // some searching should lead you to a simple pseudo-random number generator
 int generateRandomNumbers(int lowerBounds, int higherBounds) {
-    next = 1;
     next = next * 1103515245 + 12345;
-    return (unsigned int)(next/65536) % 32768;
+    return (unsigned int)(next/65536) % (higherBounds - lowerBounds);
 }
 
 //PAGEBREAK: 42
